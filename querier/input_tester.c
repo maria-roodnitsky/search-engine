d8fe3e49679f63bdd6dd6cbcde626377c8a8c3af
Maria Roodnitsky 22 <f003kgm@flume.cs.dartmouth.edu>
2020-02-24 20:05:48 -0500

    Commented and added a header for input_tester.c

--- querier/input_tester.c
+++ querier/input_tester.c
@@ -1,3 +1,12 @@
+/* 
+ * input_tester.c - CS50
+ * 
+ * Reads lines from stdin, checks for valid input, then checks for valid word order. 
+ * Prints the type for each word found. 
+ * 
+ * February 2020, Maria Roodnitsky
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -11,22 +20,22 @@ int main(void){
   char buf[200]; 
   char **words;
 
-  while ((line = fgets(buf, 200, stdin)) != NULL){
-    words = parseLineIntoWords(line);
+  while ((line = fgets(buf, 200, stdin)) != NULL){ //read a line from stdin
+    words = parseLineIntoWords(line); //parse the line into words
     
-    if (words == NULL) continue;
-    checkWordOrder(words);
+    if (words == NULL) continue; //if there was invalid input, skip this iteration
+    checkWordOrder(words); //check for valid word order; if not, an error message will print
     
     for (int i = 0; words[i] != NULL; i++){
-      NormalizeWord(words[i]);
+      NormalizeWord(words[i]); //normalize each word
       
       if (isAnd(words[i])){
-        printf("and found: %s\n", words[i]);
+        printf("and found: %s\n", words[i]); //if and, notify the user that an 'and' was found
       }
-      else if (isOr(words[i])) {
+      else if (isOr(words[i])) { //if or, notify the user that an 'or' was found
         printf("or found: %s\n", words[i]);
       }
-      else {
+      else { //if just a word, notify the user that a word was found
         printf("word found: %s\n", words[i]);
       }
     }  
